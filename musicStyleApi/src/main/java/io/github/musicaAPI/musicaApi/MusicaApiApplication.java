9d5a40102a9e8084ea97d3006b5ea080e86057b8
Crystofer Samuel <127315536+CrySamuel@users.noreply.github.com>
2024-12-29 20:44:37 -0300

    Add files via upload
    API with CRUD done (just the basics so far)

---
+++ musicStyleApi/src/main/java/io/github/musicaAPI/musicaApi/MusicaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.musicaAPI.musicaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaApiApplication.class, args);
+	}
+
+}
